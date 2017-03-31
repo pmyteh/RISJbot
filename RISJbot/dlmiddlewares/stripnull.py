c1108813c58972642b182c8374d02f440cfbb0d0
Tom Nicholls <github@tomnicholls.me.uk>
2017-03-31 19:24:18 +0100

    StripNull: only apply to TextResponse objects

--- RISJbot/dlmiddlewares/stripnull.py
+++ RISJbot/dlmiddlewares/stripnull.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
+from scrapy.http import TextResponse
 
 logger = logging.getLogger(__name__)
 
@@ -11,6 +12,9 @@ class StripNull(object):
        which really messes up the underlying lxml parsing library (which is
        C based and presumably uses null-terminated strings). The effect is that
        the page's parse tree is cut off where the NUL occurs.
+
+       We don't want to do this for possible binaries (like gzip compressed
+       sitemaps, for example).
     """
 
     def __init__(self, stats, settings):
@@ -23,6 +27,11 @@ class StripNull(object):
         return cls(crawler.stats, crawler.settings)
 
     def process_response(self, request, response, spider):
+
+        if not isinstance(response, TextResponse):
+            self.stats.inc_value('stripnull/nottext', spider=spider)
+            return response
+
         newbody = bytes([x for x in response.body if x != 0])
 
         if len(newbody) < len(response.body):
