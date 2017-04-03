0b2fad245166081bcf30a51ec37e906ac52d22db
Tom Nicholls <github@tomnicholls.me.uk>
2017-04-03 14:44:54 +0100

    CheckContent: fix

--- RISJbot/pipelines/checkcontent.py
+++ RISJbot/pipelines/checkcontent.py
@@ -5,8 +5,6 @@ logger = logging.getLogger(__name__)
 
 class CheckContent(object):
     def process_item(self, item, spider):
-        try:
-            item['bodytext']
-        except KeyError:
-            logger.error("No bodytext: {}".format(item.url))
+        if 'bodytext' not in item:
+            logger.error("No bodytext: {}".format(item.get('url']))
         return item
