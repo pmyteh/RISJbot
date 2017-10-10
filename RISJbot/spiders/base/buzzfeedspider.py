f2474ecc8cecb7e7864aececfff388d5e96dcbee
Tom Nicholls <github@tomnicholls.me.uk>
2017-10-10 16:00:18 +0100

    buzzfeed: reorganise main code into base crawler

---
+++ RISJbot/spiders/base/buzzfeedspider.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from RISJbot.spiders.newsrssfeedspider import NewsRSSFeedSpider
+from RISJbot.loaders import NewsLoader
+# Note: mutate_selector_del_xpath is somewhat naughty. Read its docstring.
+from RISJbot.utils import mutate_selector_del_xpath
+from scrapy.loader.processors import Identity, TakeFirst
+from scrapy.loader.processors import Join, Compose, MapCompose
+
+class BuzzfeedSpider(NewsRSSFeedSpider):
+    #name = 'buzzfeedXX'
+    # allowed_domains = ['buzzfeed.com']
+    #start_urls = ['https://www.buzzfeed.com/news.xml?country=XX']
+
+    # RSSFeedSpider parses the RSS feed and calls parse_page(response) as a
+    # callback for each page it finds in the feed.
+    def parse_page(self, response):
+        s = response.selector
+        # Remove any content from the tree before passing it to the loader.
+        # There aren't native scrapy loader/selector methods for this.        
+        mutate_selector_del_xpath(s, '//*[contains(@class, "print") or contains(@class, "hidden")]') # Physical print only
+
+        l = NewsLoader(selector=s)
+
+        # Remove referer params from end of URLs
+        l.add_xpath('url', 'head/link[@rel="canonical"]/@href')
+
+        # Add a number of items of data that should be standardised across
+        # providers. Can override these (for TakeFirst() fields) by making
+        # l.add_* calls above this line, or supplement gaps by making them
+        # below.
+        l.add_fromresponse(response)
+        l.add_htmlmeta()
+        l.add_schemaorg(response)
+        l.add_opengraph()
+        l.add_scrapymeta(response)
+
+        l.add_xpath('bodytext', '//div[@data-print="body"]/*[not(contains(@class, "user-bio") or contains(@class, "_shares") or contains(@class, "inline-promo"))]//text()')
+        l.add_xpath('bodytext', '//div[contains(@class, "_item_text")]//text()')
+        l.add_xpath('bodytext', '//article//*[contains(@class, "subbuzz-text") or '
+                                             'contains(@class, "subbuzz__title")]//text()')
+
+        return l.load_item()
--- RISJbot/spiders/uk/buzzfeeduk.py
+++ RISJbot/spiders/uk/buzzfeeduk.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
-from RISJbot.spiders.us.buzzfeed import BuzzfeedSpider
+from RISJbot.spiders.base.buzzfeedspider import BuzzfeedSpider
 
-# NOTE: Inherits parsing code etc. from spiders/us/buzzfeed, overriding only
-#       the name and start URL.
+# NOTE: Inherits parsing code etc., overriding only the name and start URL.
 class BuzzfeedUKSpider(BuzzfeedSpider):
     name = 'buzzfeeduk'
     # allowed_domains = ['buzzfeed.com']
     start_urls = ['https://www.buzzfeed.com/news.xml?country=uk']
+
+    def parse_page(self, response):
+        """Note: firstpubtime also fetched, but via RSS feed (which can't be
+                 contracted for)
+
+        @url https://www.buzzfeed.com/jamieross/this-scottish-politician-is-looking-for-your-support-on
+        @returns items 1
+        @scrapes bodytext bylines fetchtime headline
+        @scrapes section source summary url keywords
+        @noscrapes modtime
+        """
+        return super().parse_page(response)
 
